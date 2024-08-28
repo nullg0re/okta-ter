958dfc60636d2286ca228c7e0f958fbd97bf5334
Tony Gore <tgore@secureworks.com>
2024-08-28 14:58:15 -0500

    initial commit

---
+++ OktaCommon/Key.cs
@@ -0,0 +1,18 @@
+﻿namespace OktaCommon {
+
+    public enum KeyType {
+        Unknown,
+        DeviceAttestation,
+        UserVerification,
+        ProofOfPossession,
+        UserVerificationBioOrPin,
+        Backdoor
+    }
+
+    public class Key {
+        public string KeyId;
+        public bool Sandboxed;
+        public string Path;
+        public KeyType Type;
+    }
+}
